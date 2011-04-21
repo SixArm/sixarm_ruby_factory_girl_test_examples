0a14fae185493d3484c01fce5cb2d2f3cb0f6b21
SixArm <sixarm@sixarm.com>
2011-04-21 02:35:20 -0700

    add host_port

--- factories/pop_account.rb
+++ factories/pop_account.rb
@@ -10,7 +10,7 @@ Factory.define :pop_account do |pop_account|
   pop_account.email_address { email_address }
   pop_account.inbound_mail_host { host_name }
   pop_account.inbound_mail_port { host_port }
-  pop_account.inbound_mail_port_for_ssl { host_post }
+  pop_account.inbound_mail_port_for_ssl { host_port }
   pop_account.inbound_mail_use_ssl { boolean }
   pop_account.outbound_mail_host { host_name }
   pop_account.outbound_mail_use_authentication { boolean }
